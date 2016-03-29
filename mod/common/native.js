04cf257bb7866620536946a97c75e0d0da1db438
jianl <jianjianfengyun@126.com>
2016-03-29 11:13:20 +0800

    Update native.js
    fix

--- mod/common/native.js
+++ mod/common/native.js
@@ -3,7 +3,7 @@ var ua = require('./ua');
 module.exports = {
   openBusinessHome:function(businessCode){
     if (ua.isAndroid()){
-		   android.openBusinessHome(businessCode);
+		   window.android.openBusinessHome(businessCode);
 		}else if(ua.isIOS()){
 		   window.location.href = "bill99app://kuaiqianbao/openBusinessHome?businessCode="+businessCode;
 		}else{
