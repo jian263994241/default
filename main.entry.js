5f78c0079a0c928cf018620fb21496a918f5cafc
jianl <jianjianfengyun@126.com>
2016-05-21 15:56:11 +0800

    Update main.entry.js

--- main.entry.js
+++ main.entry.js
@@ -1,31 +1,31 @@
 var MOD;
 
-  MOD = {
-    welcome: require("./mod/welcome"),
-    index: require("./mod/index/index")
-  };
+MOD = {
+  welcome: require("./mod/welcome"),
+  index: require("./mod/index/index")
+};
 
-  window.$$ = Dom7;
+window.$$ = Dom7;
 
-  window.app = new Framework7({
-    pushState: true,
-    template7Pages: true,
-    init: false,
-    onPageInit: function(app, page) {
-      if (MOD[page.name]) {
-        return MOD[page.name](app, page);
-      }
-    },
-    onAjaxStart: function() {
-      return app.showIndicator();
-    },
-    onAjaxComplete: function() {
-      return app.hideIndicator();
+window.app = new Framework7({
+  pushState: true,
+  template7Pages: true,
+  init: false,
+  onPageInit: function(app, page) {
+    if (MOD[page.name]) {
+      return MOD[page.name](app, page);
     }
-  });
+  },
+  onAjaxStart: function() {
+    return app.showIndicator();
+  },
+  onAjaxComplete: function() {
+    return app.hideIndicator();
+  }
+});
 
-  app.session = require('./mod/common/session');
+app.session = require('./mod/common/storage').session;
 
-  window.mainView = app.addView('.view-main');
+window.mainView = app.addView('.view-main');
 
-  app.init();
+app.init();
