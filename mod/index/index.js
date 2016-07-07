d248a08018c7a7b3d262a4da0dbc6ebba356fb79
jianl <jianjianfengyun@126.com>
2016-07-07 11:04:02 +0800

    Update index.js

--- mod/index/index.js
+++ mod/index/index.js
@@ -1,10 +1,7 @@
 var tpl = require('./index.tpl.html');
 var tplFn = Template7.compile(tpl);
 
-var helper, service;
-
-service = require("../common/service");
-helper = require("../common/helper");
+var service = require("../common/service");
 
 module.exports = function(app, page) {
   var container;
