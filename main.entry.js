35cdb6f146ee9ecd0e0f4b457507d18cc72ab808
jianl <jianjianfengyun@126.com>
2016-07-18 21:02:45 +0800

    Update main.entry.js

--- main.entry.js
+++ main.entry.js
@@ -1,6 +1,5 @@
-var MOD;
-
-MOD = {
+var util = require('./mod/common/util');
+var MOD = {
   index: require("./mod/index/index")
 };
 
@@ -18,7 +17,7 @@ window.app = new Framework7({
   },
   onPageBeforeAnimation: function(app, page) {
     var dataset = Dom7(page.container).dataset();
-    app.title(dataset.title);
+    util.setTitle(dataset.title);
   },
   preprocess: function(content, url, next) {
     app.H5login.login(function() {
@@ -29,28 +28,6 @@ window.app = new Framework7({
 
 app.H5login = require('./mod/common/H5login')(app);
 app.session = require('./mod/common/storage').session;
-
-app.title = function(title) {
-  document.title = title;
-  try {
-    kuaiqian.setPageTitle({
-      title: title
-    });
-  } catch (e) {
-    // alert(e)
-  }
-};
-app.env = function() {
-  var ua = navigator.userAgent.toLowerCase();
-  return {
-    isWeixin: ua.match(/MicroMessenger/i) == 'micromessenger',
-    iOS: ua.indexOf('iphone') > -1 || ua.indexOf('ipad') > -1 || ua.indexOf('ipod') > -1,
-    Android: ua.indexOf('android') > -1,
-    KQ: ua.indexOf('kuaiqianbao') > -1,
-    FeiFan: ua.indexOf('feifan') > -1
-  };
-}();
-
 
 window.mainView = app.addView('.view-main', {
   domCache: true
