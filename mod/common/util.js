f4af1c02c3669e8955edfcec928d68155234640c
jianl <jianjianfengyun@126.com>
2016-08-17 19:10:15 +0800

    Update util.js

--- mod/common/util.js
+++ mod/common/util.js
@@ -1,5 +1,3 @@
-
-
 module.exports = {
   // 验证登录密码 前提已满足只有特殊字符 字母和数字
   valiPassword: function(param) {
@@ -29,7 +27,7 @@ module.exports = {
     }, 1000, btn);
     return btn;
   },
-  setTitle: function(title){
+  setTitle: function(title) {
     document.title = title;
     try {
       KQB.native("setPageTitle", {
@@ -106,6 +104,111 @@ module.exports = {
   inputMaxLength: function(input, maxLength) {
     $$(input).on('input', function(e) {
       e.target.value = maxLength(e.target.value, maxLength)
+    });
+  },
+  /**
+   * @prams type: 'get','post'
+   * @prams opt:obj {
+   *  url:str 请求链接
+   *  data:obj (选填) ajax 数据
+   *  codes:arr (选填) 执行callback的先决条件 默认:["00"]
+   *  title:str (选填) 等待标题 默认"请等待..."
+   *  callback:fn 回调函数
+   *  loginToken:bool, (选填) 值为 true 的时候  请求header 带入 loginToken
+   *  timeout:num (选填) 超时时间 , 默认 0
+   *  errorCallback:fn (选填) 错误回调函数
+   *}
+   **/
+  ajax: function(type, opt) {
+    var app = Framework7.prototype.constructor();
+    var type = type.toLocaleUpperCase();
+    var codes = opt.codes || ["00"];
+    var title = opt.title || "请等待..."
+    var timeout = opt.timeout || 0;
+    var ajaxOpt = {
+      url: opt.url,
+      method: type,
+      timeout: timeout,
+      success: successHandle,
+      error: errorHandle,
+      beforeSend: beforeSend,
+      complete: complete
+    };
+    if (opt.data) {
+      if (type === "POST") {
+        ajaxOpt.contentType = 'application/json;charset=UTF-8';
+        ajaxOpt.data = JSON.stringify(opt.data);
+      } else {
+        ajaxOpt.data = opt.data;
+      }
+    }
+
+    if (opt.loginToken) {
+      ajaxOpt.headers = {
+        Authorization: sessionStorage.loginToken
+      }
+    };
+    //other token
+    if (opt.token) {
+      ajaxOpt.headers = {
+        Authorization: sessionStorage.token
+      }
+    };
+
+    function beforeSend(xhr) {
+      return app.showPreloader(title);
+    };
+
+    function complete(xhr, status) {
+      return app.hidePreloader();
+    };
+
+    function successHandle(data, status, xhr) {
+      var codeIn = false;
+      var data = JSON.parse(data);
+      console.groupCollapsed(xhr.requestParameters.method, xhr.requestUrl);
+      console.log('headers:', xhr.requestParameters.headers);
+      console.log('request:', typeof xhr.requestParameters.data == 'string' ? JSON.parse(xhr.requestParameters.data) : xhr.requestParameters.data);
+      console.log('response:', data);
+      console.groupEnd();
+      codes.forEach(function(code, index) {
+        if (data.errCode === code) {
+          codeIn = true;
+        }
+      });
+
+      if (codeIn) {
+        return opt.callback(data);
+      } else {
+        return app.alert(data.errMsg);
+      }
+    };
+
+    function errorHandle(xhr, status) {
+      if (opt.errorCallback) {
+        return opt.errorCallback(xhr, status);
+      };
+      console.log('请求失败:', opt.url);
+      return app.toast('网络状况不太好,稍后再试哦~');
+    };
+
+    Dom7.ajax(ajaxOpt);
+  },
+  loadPage: function(url, success) {
+    var app = Framework7.prototype.constructor();
+    Dom7.ajax({
+      method: 'GET',
+      url: url,
+      beforeSend: function() {
+        app.showIndicator();
+      },
+      complete: function() {
+        app.hideIndicator();
+      },
+      success: success,
+      error: function() {
+        app.toast('网络异常');
+      }
     });
   }
 
