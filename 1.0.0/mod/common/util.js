b2063e276a14999319766c7f5295813d2275f09e
jianl <jianjianfengyun@126.com>
2016-09-08 14:52:26 +0800

    Update util.js

--- 1.0.0/mod/common/util.js
+++ 1.0.0/mod/common/util.js
@@ -219,6 +219,19 @@ module.exports = {
   },
   enableBounce: function() {
     document.body.removeEventListener('touchmove', bodyScroll, false);
+  },
+  loadReactPage: function(opt, view) {
+    var page = document.createElement('div');
+    var pushState = opt.pushState == undefined ? true : false;
+    var animatePages = opt.animatePages == undefined ? true : false;
+    var props = opt.props || {}
+    var view = view || app.getCurrentView();
+    ReactDOM.render(React.createElement(opt.rdom, props), page);
+    view.router.load({
+      content: page,
+      pushState: pushState,
+      animatePages: animatePages
+    });
   }
 
 }
