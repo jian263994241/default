07d5f2945ae7cf1b3fa384cbf8857633b3e23a1d
jianl <jianjianfengyun@126.com>
2017-05-22 10:33:02 +0800

    ..

--- src/app.js
+++ src/app.js
@@ -14,23 +14,23 @@ const router = {
 };
 
 
-render((
-  <div className="views">
-    <div className="view view-main">
-      <div className="pages"></div>
-    </div>
-  </div>
-), $('.framework7-root')[0], ()=>{
-
-  window.app = new Framework7({
-    reactComponent: router,
-    root:'.framework7-root'
-  });
-
-  app.mainView = app.addView('.view-main');
-
-  if (location.hash == '') {
-    app.mainView.router.load({url: 'index/demo', animatePages: false, reload: true});
-  }
-
-})
+// render((
+//   <div className="views">
+//     <div className="view view-main">
+//       <div className="pages"></div>
+//     </div>
+//   </div>
+// ), $('.framework7-root')[0], ()=>{
+//
+//   window.app = new Framework7({
+//     reactComponent: router,
+//     root:'.framework7-root'
+//   });
+//
+//   app.mainView = app.addView('.view-main');
+//
+//   if (location.hash == '') {
+//     app.mainView.router.load({url: 'index/demo', animatePages: false, reload: true});
+//   }
+//
+// })
