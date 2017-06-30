c822d63bb0faaf47032db05c501fcc26706c9445
jianl <jianjianfengyun@126.com>
2017-06-30 09:52:25 +0800

    ..

--- c-conf.js
+++ c-conf.js
@@ -19,16 +19,3 @@ fis.match('app.js', {
 })
 
 
-const exec = require('child_process').exec;
-
-fis.on('release:end', function(file){
-  if(fis.project.currentMedia() === 'prod2'){
-    exec('tar -cvf release.zip ' + '_build/', (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(stdout);
-    })
-  }
-})
