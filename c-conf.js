232ef68b8d7e46716a16ead85963b46d06994e02
jianl <jianjianfengyun@126.com>
2017-08-01 13:44:41 +0800

    .

--- c-conf.js
+++ c-conf.js
@@ -9,14 +9,57 @@ const shims = {
 
 fis.match('app.js', {
   parser: fis.plugin('browserify',{
-    shims: shims
+    shims: shims,
+    insertGlobalVars: {
+      ACTIVECODE: function(file, basedir){
+        return JSON.stringify('st02Code');
+      }
+    }
   }),
   guard: false
 })
 
-fis.media('prod2').match('app.js', {
+fis.media('st02').match('app.js', {
   parser: fis.plugin('browserify',{
-    shims: shims
+    shims: shims,
+    insertGlobalVars: {
+      ACTIVECODE: function(file, basedir){
+        return JSON.stringify('st02Code');
+      }
+    }
   }),
   guard: true  // 加密
+})
+
+fis.media('prod2').match('app.js', {
+  parser: fis.plugin('browserify',{
+    shims: shims,
+    insertGlobalVars: {
+      ACTIVECODE: function(file, basedir){
+        return JSON.stringify('prod2Code');
+      }
+    }
+  }),
+  guard: true  // 加密
+})
+
+
+fis.match('default.html',{
+  parser: fis.plugin('getconf',{
+    confFile: {
+      res: {
+        react: 'https://img.99bill.com/seashell/webapp/lib/react/15.6.1/react.js'
+      }
+    }
+  })
+})
+
+fis.media('prod2').match('default.html',{
+  parser: fis.plugin('getconf',{
+    confFile: {
+      res: {
+        react: 'https://img.99bill.com/seashell/webapp/lib/react/15.6.1/react.min.js'
+      }
+    }
+  })
 })
