58baafcbf0f9eb1973c4375d95837df472455637
jianl <jianjianfengyun@126.com>
2017-10-18 10:46:07 +0800

    ..

--- src/mod/common/ajax.js
+++ src/mod/common/ajax.js
@@ -1,4 +1,4 @@
-import {kq} from 'wonder'
+import {Modal} from 'wonder'
 
 let domain = "";
 
@@ -12,12 +12,17 @@ if (location.host === 'www.99bill.com') {
 }
 
 const ajax = function({url, prefix = '/coc-bill-api', ...rest}){
-  const api = kq.api({
+  const api = kqlib.api({
     url: domain + prefix + url,
     business: 'MEMBER-BASE',
     ...rest
   });
   return api;
+}
+
+window.onAPIErrorHandler = function(data, status, xhr){
+  //全局错误提示
+  Modal.toast(data.errMsg || data.responseMsg);
 }
 
 
