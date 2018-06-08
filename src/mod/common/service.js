e08cc6a627c5ee0600f72cd9203f0d7b2e90a0ad
jianl <jianjianfengyun@126.com>
2018-06-08 14:52:12 +0800

    ..merge v2

--- src/mod/common/service.js
+++ src/mod/common/service.js
@@ -1,39 +1,62 @@
-import {kq} from 'wonder'
+import ajax from './ajax';
+import device from 'kqjs/lib/env';
 
-const encryptByDES = window.encryptByDES;
+// 运行环境
+const __env = window.__ENV__ ? window.__ENV__.name : 'prod';
 
-let domain = "";
-
-if (location.host === 'sandbox.99bill.com') {
-  //sandbox
-  domain = 'https://sandbox.99bill.com';
+// 域名
+let domain = {
+  ffan: 'https://api.ffan.com',
+  fcc: 'https://fcc-fql-api.ffan.com',
+  ebd: 'https://ebd.99bill.com',
+  jr: 'https://jr.99bill.com',
+  rip: 'https://rip.99bill.com'
 }
 
-if (location.host === 'www.99bill.com') {
-  //sandbox
-  domain = 'https://ebd.99bill.com';
+// 本地服务做了代理
+if (location.port === '8080') {
+    domain.ebd = '';
+    domain.jr = '';
+    domain.rip = '';
 }
 
-const ajax = function({url, ...other}){
-  const api = kq.api({
-    url: domain + url,
-    business: 'MEMBER-BASE',
-    ...other
-  });
-  return api;
+// fcc 测试环境域名
+if (__env !== 'prod') {
+    domain.fcc = 'https://fcc-fql-api.uat.ffan.com';
 }
 
-const API = {
-  login: '/coc-bill-api/mam/3.0/members/password/login', //登录
-}
+// 服务API
+const API_URL = {
+  // 查询资讯列表页接口 http://192.168.127.151:8080/workspace/myWorkspace.do?projectId=48#2459
+  queryNewsList: domain.jr + '/fpd-platform/common/news/queryNewsList.htm',
+};
 
+/**
+ * 查询资讯列表页接口
+ */
+const queryNewsList = (type=3, pageSize=3)=>{
+  let orderSource = 2;
 
-export function login (data) {
-  const idContent = encryptByDES(data.idContent);
-  const password = encryptByDES(data.password);
+  if(device.KQ){
+      orderSource = 3;
+  }else if(device.FeiFan){
+      orderSource = 6;
+  }else if(device.Weixin){
+      orderSource = 8;
+  }
+
   return ajax({
-    method: 'post',
-    url: API.login,
-    data:{idContent, password}
+      method: 'post',
+      url: API_URL.queryNewsList,
+      data: {
+          type,
+          pageSize
+      },
+      token: false,
+      errCode: ['0000']
   });
+}
+
+export default {
+  queryNewsList
 }
